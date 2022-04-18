7f49f8289e2c9c419392d9a818f9a4eaf50510f4
ChrisSamHarris <christopherharris1995@ymail.com>
2022-04-18 09:52:01 +0100

    Uploading Web Development Training

---
+++ 29-33JavaScriptBasics/operations.js
@@ -0,0 +1,30 @@
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4); //10 / 4 = 2.5 => 2 * 4 > Remainder: 2
+console.log(10 % 3); //Expected remainder = 1 
+
+console.log((10 + 3 - 5) * 10); //Brackets and bracket placing is important 
+
+let result = (10 + 3 - 5) * 10; 
+result = 10 * 4;
+result++;
+result--;
+
+result += 5; // result = result + 5 
+result -= 5; // result = result - 5 
+result /= 5; // result = result / 5 
+result *= 5; // result = result * 5 
+console.log(result);
+
+console.log('Chris' + ' ' + 'Harris'); // concatonate a string - cant minus (-) strings however
+
+let userName = 'Christopher';
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+
+let hobbies = ["Sports", "Study"];
+console.log(hobbies.length); 
+console.log(hobbies);
---
+++ 33-37JSwwDOM/app.js
@@ -0,0 +1,6 @@
+console.dir(document);
+
+//this wont work if the JavaScript code is loaded prior to html
+document.body.children[1].children[0].href = 'https://www.google.com';
+
+//DOM = Document Object Model›
