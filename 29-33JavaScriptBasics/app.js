b89fbcaca407ad8e7706cadbe1424f68b540576d
Christopher Harris <christopher.harris@moneysupermarket.com>
2022-07-02 11:00:03 +0100

    Re-working on Web Development

--- 29-33JavaScriptBasics/app.js
+++ 29-33JavaScriptBasics/app.js
@@ -1,50 +1,8 @@
-let greetingText = "Hi I'm is Chris!";
-/* only use the let keyword when you create and introduce a variable  */
-let age = 26;
-let hobbies = ["Sports", "Cooking", "Reading"];
-let job = {
-  title: "Developer",
-  place: "Manchester, UK",
-  salary: 150000,
-};
+let greetingText = 'Hi, this is a variable created using JavaScript';
+let numericVariable = 32;
+alert('Hi, this is a \'string\' created using JavaScript');
+alert(greetingText);
+greetingText = 'boop';
+alert(greetingText);
+alert(numericVariable);
 
-let totalAdultYears;
-
-function calculateAdultYears(userAge) {
-    /* adultYears = age - 18; */
-    /* let result;
-    result = userAge - 18;
-    return result; */
-    
-    return userAge - 18;
-    /* DO NOT DEFINE a variable within a function */
-}
-
-totalAdultYears = calculateAdultYears(age);
-/* adultYears = age - 18; */
-/* alert(totalAdultYears); */
-console.log(totalAdultYears);
-
-/* location of the input variable matters! */
-age = 45;
-totalAdultYears = calculateAdultYears(age);
-alert(totalAdultYears);
-console.log(totalAdultYears);
-
-/* alert(greetingText); */
-/* alert("test string"); */
-/* alert(32); */
-/* alert(age); */
-/* alert(hobbies[0]); */
-/* alert(job.title); */
-
-let person = {
-    name: 'Chris', //Property
-    greet() { //Method
-        /* alert('Hello'); */
-        console.log("Hello!");
-    }
-};
-
-//access a function in an object with .notation
-person.greet();
